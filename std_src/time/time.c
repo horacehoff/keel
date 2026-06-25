e3bccdd4f2b6c79b44fec14d7e2f9b9335696dc2
just_a_mango <horace.hoff@gmail.com>
2026-06-25 18:30:16 +0200

    Updated time lib

--- std_src/time/time.c
+++ std_src/time/time.c
@@ -1,5 +1,3 @@
-// WORK IN PROGRESS!!
-
 #ifdef _WIN32
 #define EXPORT __declspec(dllexport)
 #else
@@ -8,4 +6,11 @@
 
 #include <time.h>
 
-EXPORT int get_time(void) { return (int)time(NULL); }
+EXPORT int now(void) { return (int)time(NULL); }
+EXPORT const char *format(int timestamp, const char *fmt) {
+  static char buffer[128];
+  time_t t = (time_t)timestamp;
+  struct tm *info = localtime(&t);
+  strftime(buffer, 128, fmt, info);
+  return buffer;
+}

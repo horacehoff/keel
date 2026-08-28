81e6c7ef4356e60f844bf96920ba733c575dcb81
just_a_mango <horace.hoff@gmail.com>
2026-08-28 17:28:34 +0200

    Improved GC performance and memory usage, switched to a bitset for live heap objects

--- src/vm/gc.rs
+++ src/vm/gc.rs
@@ -1,3 +1,5 @@
+use fixedbitset::FixedBitSet;
+
 use crate::data::Data;
 use crate::vm::{MapPool, ObjectPool, RegisterFile, StringPool};
 use std::collections::HashMap;
@@ -6,9 +8,9 @@ pub struct Gc {
     pub free_arrays: Vec<u32>,
     pub free_maps: Vec<u32>,
     pub free_strings: Vec<u16>,
-    pub array_live: Vec<bool>,
-    pub map_live: Vec<bool>,
-    pub string_live: Vec<bool>,
+    pub array_live: FixedBitSet,
+    pub map_live: FixedBitSet,
+    pub string_live: FixedBitSet,
     pub stack: Vec<Data>,
     pub array_threshold: u32,
     pub map_threshold: u32,
@@ -16,11 +18,11 @@ pub struct Gc {
 }
 
 #[inline(always)]
-fn mark_data(d: Data, string_live: &mut [bool], stack: &mut Vec<Data>) {
+fn mark_data(d: Data, string_live: &mut FixedBitSet, stack: &mut Vec<Data>) {
     if d.is_heap() {
         if d.is_large_str() {
             unsafe {
-                *string_live.get_unchecked_mut(d.get_str_pool_id()) = true;
+                string_live.insert_unchecked(d.get_str_pool_id());
             }
         } else if !d.is_function() {
             // ARRAY, STRUCT, MAP
@@ -35,9 +37,9 @@ impl Gc {
             free_arrays: Vec::with_capacity(obj_pool.len()),
             free_maps: Vec::with_capacity(map_pool.len()),
             free_strings: Vec::with_capacity(str_pool.len()),
-            array_live: Vec::new(),
-            map_live: Vec::new(),
-            string_live: Vec::new(),
+            array_live: FixedBitSet::new(),
+            map_live: FixedBitSet::new(),
+            string_live: FixedBitSet::new(),
             stack: Vec::with_capacity(obj_pool.len()),
             array_threshold: 256,
             map_threshold: 256,
@@ -54,11 +56,11 @@ impl Gc {
         recursion_stack: &RegisterFile,
     ) {
         self.array_live.clear();
-        self.array_live.resize(obj_pool.len(), false);
+        self.array_live.grow(obj_pool.len());
         self.map_live.clear();
-        self.map_live.resize(map_pool.len(), false);
+        self.map_live.grow(map_pool.len());
         self.string_live.clear();
-        self.string_live.resize(str_pool_len, false);
+        self.string_live.grow(str_pool_len);
 
         // Find all used strings, arrays, maps, and structs
         for data in registers.0.iter().chain(recursion_stack.0.iter()) {
@@ -67,21 +69,19 @@ impl Gc {
 
         while let Some(d) = self.stack.pop() {
             if d.is_map() {
-                let is_live = unsafe { self.map_live.get_unchecked_mut(d.as_map()) };
-                if *is_live {
+                let was_live = unsafe { self.map_live.put_unchecked(d.as_map()) };
+                if was_live {
                     continue;
                 }
-                *is_live = true;
                 for (k, v) in &map_pool[d.as_map()] {
                     mark_data(*k, &mut self.string_live, &mut self.stack);
                     mark_data(*v, &mut self.string_live, &mut self.stack);
                 }
             } else {
-                let is_live = unsafe { self.array_live.get_unchecked_mut(d.as_array()) };
-                if *is_live {
+                let was_live = unsafe { self.array_live.put_unchecked(d.as_array()) };
+                if was_live {
                     continue;
                 }
-                *is_live = true;
                 for e in &obj_pool[d.as_array()] {
                     mark_data(*e, &mut self.string_live, &mut self.stack);
                 }
@@ -107,10 +107,8 @@ impl Gc {
             self.mark(obj_pool, map_pool, str_pool.len(), registers, recursion_stack);
             self.free_arrays.clear();
             // Mark as free any array that isn't referenced by a register
-            for (i, is_array_alive) in self.array_live.iter().enumerate() {
-                if !is_array_alive {
-                    self.free_arrays.push(i as u32);
-                }
+            for i in self.array_live.zeroes() {
+                self.free_arrays.push(i as u32);
             }
             if let Some(id) = self.free_arrays.pop() {
                 obj_pool[id as usize].clear();
@@ -138,10 +136,8 @@ impl Gc {
             self.map_threshold *= 2;
             self.mark(obj_pool, map_pool, str_pool.len(), registers, recursion_stack);
             self.free_maps.clear();
-            for (i, is_map_alive) in self.map_live.iter().enumerate() {
-                if !is_map_alive {
-                    self.free_maps.push(i as u32);
-                }
+            for i in self.map_live.zeroes() {
+                self.free_maps.push(i as u32);
             }
             if let Some(id) = self.free_maps.pop() {
                 map_pool[id as usize].clear();
@@ -164,10 +160,8 @@ impl Gc {
         self.string_threshold *= 2;
         self.mark(obj_pool, map_pool, str_pool_len, registers, recursion_stack);
         self.free_strings.clear();
-        for (i, is_str_alive) in self.string_live.iter().enumerate() {
-            if !is_str_alive {
-                self.free_strings.push(i as u16);
-            }
+        for i in self.string_live.zeroes() {
+            self.free_strings.push(i as u16);
         }
     }
 
